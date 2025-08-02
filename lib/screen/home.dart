c5d21e3e96ab1a45fd1d8dc9fb4c7aa90c186c53
ReDo Programmer <redoprogrammer@gmail.com>
2025-08-02 22:32:55 +0700

    display user info from url

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,11 +11,28 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('REST API called'),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context,index){
+          final user = users[index];
+          final name = '${user['name']['title']} ${user['name']['first']} ${user['name']['last']}';
+          final email = user['email'];
+          final avatar = user['picture']['thumbnail'];
+          return ListTile(
+            leading: CircleAvatar(
+                child: Text('${++index}'),
+            ),
+            title: Text(email),
+            subtitle: Text(name),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: fetchUsers,
@@ -21,7 +40,18 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void fetchUsers(){
+  void fetchUsers() async{
     print('fetchUsers called');
+    const url = 'https://randomuser.me/api/?results=20';
+    final uri = Uri.parse(url);
+    final response  = await http.get(uri);
+    final body = response.body;
+    final jsResult = jsonDecode(body);
+    setState(() {
+      users = jsResult['results'];
+    });
+    print('fetch users completed');
+
+
   }
 }
